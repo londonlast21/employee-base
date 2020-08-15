3487b0c2c1427508655b7f24d068950b922186ad
jack <biffisbuffm@gmail.com>
2020-08-14 20:56:12 -0500

    initial

---
+++ db/database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "londonlast21",
+    password: "Wigolettawins1!"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    // con.query("CREATE DATABASE employees", function (err, result) {
+    //   if (err) throw err;
+    //   console.log("Database created");
+    // });
+});
+module.exports = con;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 // npm to require
-const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const con = require('./db/database');
 
-// create the connection to database
+// create the connection to localHost
+const PORT = process.env.PORT || 3001;
+
+// Start server after DB connection
+con.on('open', () => {
+    {
+      console.log(`Server running on port ${PORT}`);
+    };
+});
