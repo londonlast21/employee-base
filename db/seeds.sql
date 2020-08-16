9343e522c198e0f8fae28f0befc6b0851cd42b5f
jack <biffisbuffm@gmail.com>
2020-08-15 22:16:46 -0500

    data for dept

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+/* department table data */
+INSERT INTO department (name)
+VALUES
+('Sociology'),
+('Math'),
+('English'),
+('History'),
+('Science'),
+('Phys. Ed'),
+('Drama');
+
+
+
+-- /* role table data */
+-- INSERT INTO role (title, salary, )
+-- VALUES
+
+/* employee table data */
