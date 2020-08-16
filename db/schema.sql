47b39607a41a0dc3f0f31aa993678c259679434c
jack <biffisbuffm@gmail.com>
2020-08-16 01:28:04 -0500

    cant make this work need sleep

--- db/schema.sql
+++ db/schema.sql
@@ -13,15 +13,16 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INTEGER UNSIGNED,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER UNSIGNED NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
     
 );
 
+
 CREATE TABLE employee (
-    employeeID INTEGER PRIMARY KEY,
+    employeeID INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED,
