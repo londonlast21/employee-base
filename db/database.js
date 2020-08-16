4c100ebfee9eb3ad3d7efdb665fb485f1890cb93
jack <biffisbuffm@gmail.com>
2020-08-16 12:46:33 -0500

    this is stupid

--- db/database.js
+++ db/database.js
@@ -4,16 +4,7 @@ const con = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Wigolettawins1!"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-
-  // then create my database
-  con.query("CREATE DATABASE employeeBank", function (err, result) {
-      if (err) throw err;
-    
-  });
-  
 });
+  
+
 module.exports = con;
