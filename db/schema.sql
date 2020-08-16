d1e99ed70cc69f318ca911aef4dc07a5fc5f5978
jack <biffisbuffm@gmail.com>
2020-08-15 21:49:35 -0500

    tables except employees

--- db/schema.sql
+++ db/schema.sql
@@ -11,17 +11,18 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
+    roleID INTEGER PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INTEGER NOT NULL
 );
 
+/* this table is only one not generating; error with roleid?
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED,
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES role(roleID) ON DELETE CASCADE,
     manager_id INTEGER 
 );
