a7af294ca07cbf6272804f2c696f1bf570c07356
jack <biffisbuffm@gmail.com>
2020-08-16 00:16:07 -0500

    creates tables

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE role (
 
 /*this table is only one not generating; error with roleid?*/
 CREATE TABLE employee (
-    employee_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED,
