625fda4a580ba61a77f3c326c4d937b67aabcb25
jack <biffisbuffm@gmail.com>
2020-08-16 10:30:47 -0500

    tables all generate; data for dept ok nothing else ok

--- db/schema.sql
+++ db/schema.sql
@@ -9,25 +9,36 @@ CREATE TABLE department (
     
 
 );
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+    
+
+);
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER UNSIGNED NOT NULL,
-    CONSTRAINT uc_department UNIQUE (department_id)
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER UNSIGNED NOT NULL,
+    CONSTRAINT uc_department UNIQUE (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
-    
+    
 );
 
 
 CREATE TABLE employee (
-    employeeID INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER UNSIGNED,
-    CONSTRAINT uc_role UNIQUE (role_ID),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INTEGER 
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER UNSIGNED,
+    CONSTRAINT uc_role UNIQUE (role_ID),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER 
 );
