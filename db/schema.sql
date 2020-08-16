2ae7d2ed83f0bc5f1aeb342a2dc7ae66ba882888
jack <biffisbuffm@gmail.com>
2020-08-16 10:33:31 -0500

    edndnskjhdafg

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,6 @@ DROP TABLE IF EXISTS employee;
 
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-    
-
-);
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
-
-
-CREATE TABLE department (
     id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
     
@@ -26,7 +15,6 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER UNSIGNED NOT NULL,
-    CONSTRAINT uc_department UNIQUE (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
     
@@ -38,7 +26,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED,
-    CONSTRAINT uc_role UNIQUE (role_ID),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INTEGER 
 );
