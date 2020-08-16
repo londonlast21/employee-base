46ed8bef4bc8816adb2c056ec259e7d855661b76
jack <biffisbuffm@gmail.com>
2020-08-15 20:48:58 -0500

    routes

--- routes/apiRoutes/departmentRoute.js
+++ routes/apiRoutes/departmentRoute.js
@@ -1,4 +1,11 @@
+const inquirer = require('inquirer');
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/database');
+
+// api route to get cli input
+router.get('/departments', (req, res) => {
+    
+})
 
 module.exports = router;
--- routes/apiRoutes/roleRoute.js
+++ routes/apiRoutes/roleRoute.js
@@ -1,4 +1,6 @@
+const inquirer = require('inquirer');
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/database');
 
 module.exports = router;
